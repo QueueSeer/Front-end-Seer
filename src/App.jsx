8148d399c9d5a6182b3951b11a51ba7a69c36048
SURANGKANANG <ploylol3966@gmail.com>
2024-11-13 03:47:01 +0700

    create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+// App.js
 import React from "react";
-import Navbar from "./components/navbar"; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Login from "./pages/Login/Login";
 import './index.css';
 
 export default function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="/" element={<h1>หน้าแรก</h1>} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Images from "../../assets";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -21,15 +22,14 @@ export default function Navbar() {
           <li><a href="#home">หน้าหลัก</a></li>
           <li><a href="#packages">แพ็กเกจ</a></li>
           <li><a href="#auction">ประมูล</a></li>
-          <li><a href="#articles">บทความ</a></li>
         </ul>
       </div>
 
       {/* ปุ่มเข้าสู่ระบบและลงทะเบียน */}
       <div className="flex-none flex gap-2">
-      <button className="btn btn-outline btn-sm text-white hover:bg-opacity-20" style={{ color: "#8677A7", borderColor: "#8677A7" }}>
-          เข้าสู่ระบบ
-        </button>
+      <Link to="/login" className="btn btn-outline btn-sm text-white hover:bg-opacity-20" style={{ color: "#8677A7", borderColor: "#8677A7" }}>
+    เข้าสู่ระบบ
+  </Link>
         <button className="btn btn-sm text-white" style={{ backgroundColor: "#8677A7" }}>
           ลงทะเบียน
         </button>
