ba26adcd0e5632b9d02018c477a920bff4a5125d
SURANGKANANG <ploylol3966@gmail.com>
2024-11-13 22:14:24 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 // App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
 import Login from "./pages/Login/Login";
 import './index.css';
 
 export default function App() {
   return (
     <Router>
-   
       <Routes>
+        {/* เส้นทางสำหรับหน้าแรก */}
+        <Route path="/" element={<Login />} />
+        {/* เส้นทางสำหรับหน้า /Login */}
         <Route path="/Login" element={<Login />} />
       </Routes>
     </Router>
