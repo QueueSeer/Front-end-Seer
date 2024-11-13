a6e2a24289ec03c830a2ced9419a0749e8995cad
SURANGKANANG <ploylol3966@gmail.com>
2024-11-13 23:56:54 +0700

    create register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 // App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Login/Register"; // เปลี่ยนชื่อให้ตรงกับหน้า Register
 import './index.css';
 
 export default function App() {
   return (
     <Router>
-   
       <Routes>
-        <Route path="/Login" element={<Login />} />
+        {/* เส้นทางสำหรับหน้า Login */}
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        {/* เส้นทางสำหรับหน้า Register */}
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
