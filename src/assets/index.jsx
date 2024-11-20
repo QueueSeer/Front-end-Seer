b969a53edf8410a8de0999809595d9d8942b8b66
SURANGKANANG <ploylol3966@gmail.com>
2024-11-20 23:33:32 +0700

    icon

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -4,6 +4,17 @@ import eyeIcon from './icon/login/eyeIcon.png';
 import keyIcon from './icon/login/keyIcon.png';
 import eyeopenIcon from './icon/login/eyeopenIcon.png';
 import letterIcon from './icon/login/letterIcon.png';
+import iconCheckedUserMale from './icon/membership/iconCheckedUserMale.png';
+import iconCheckedUserMale1 from './icon/membership/iconCheckedUserMale1.png';
+import iconCheckFile from './icon/membership/iconCheckFile.png';
+import iconCheckFile1 from './icon/membership/iconCheckFile1.png';
+import iconMarbel from './icon/membership/iconmarbel.png';
+import iconStart from './icon/membership/iconStart.png';
+import iconProfiles from './icon/membership/iconProfiles.png';
+import iconProfiles1 from './icon/membership/iconProfiles1.png';
+import iconcalendar from './icon/membership/iconcalendar.png';
+
+
 const images = {
     logo,
     marble,
@@ -11,7 +22,16 @@ const images = {
     eyeopenIcon,
     keyIcon,
     letterIcon,
-    
-  };
-  
-  export default images;
+    iconCheckedUserMale,
+    iconCheckedUserMale1,
+    iconCheckFile,
+    iconCheckFile1,
+    iconMarbel,
+    iconStart,
+    iconProfiles,
+    iconProfiles1,
+    iconcalendar,
+
+};
+
+export default images;
