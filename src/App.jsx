2512a2fda978d0a4be97ade222f557bdf15d4712
KerkkaiwanMam <kerkkaiwan.ks@gmail.com>
2025-01-20 23:13:39 +0700

    Update layout ของแต่ละ หน้า และ สร้างกรา์ดบนจองคิวทันที

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,46 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
-import NotificationDetail from "./pages/Notification/NotificationDetail"; // คอมโพเนนต์สำหรับแสดงรายละเอียดการแจ้งเตือน
+import NotificationDetail from "./pages/Notification/NotificationDetail";
 import Appointment from "./pages/Appointment/Appointment";
 import DetailsAppointment from "./pages/Appointment/DetailsAppointment";
-
-import "./index.css";
 import Package from "./pages/Package";
 import Drafted from "./pages/Package/DraftPackage/Drafted";
 import Published from "./pages/Package/DraftPackage/Published";
 import HiddenPackage from "./pages/Package/DraftPackage/HiddenPackage";
 import FortuneNow from "./pages/FortuneNow";
+import DraftedFortuneNow from "./pages/FortuneNow/DraftedFortuneNow/DraftedFortuneNow";
 
+import "./index.css";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Profile & Authentication */}
         <Route path="/" element={<Profile />} />
         <Route path="/profile" element={<Profile />} />
-
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/notifications"
-          element={<p className="p-4">หน้าการแจ้งเตือนทั้งหมด</p>}
-        />
-        {/* เส้นทางสำหรับรายละเอียดการแจ้งเตือน */}
+
+        {/* Notifications */}
+        <Route path="/notifications" element={<p className="p-4">หน้าการแจ้งเตือนทั้งหมด</p>} />
         <Route path="/notification/:id" element={<NotificationDetail />} />
+
+        {/* Appointments */}
         <Route path="/appointment" element={<Appointment />} />
         <Route path="/appointment/:id" element={<DetailsAppointment />} />
+
+        {/* Package Management */}
         <Route path="/package" element={<Package />} />
         <Route path="/package/drafted" element={<Drafted />} />
         <Route path="/package/published" element={<Published />} />
-        <Route path="/package/hiddenPackage" element={<HiddenPackage />} />
-        <Route path="/FortuneNow" element={<FortuneNow />} />
+        <Route path="/package/hidden" element={<HiddenPackage />} />
 
-
-
+        {/* FortuneNow */}
+        <Route path="/fortuneNow" element={<FortuneNow />} />
+        <Route path="/fortuneNow/drafted" element={<DraftedFortuneNow />} />
       </Routes>
     </Router>
   );
