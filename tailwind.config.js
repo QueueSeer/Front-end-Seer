5d16a900f4465c5d5a81ec2cbd084e68a9ba203f
KerkkaiwanMam <kerkkaiwan.ks@gmail.com>
2025-03-27 18:25:49 +0700

    margin profile5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -18,6 +18,9 @@ module.exports = {
       },
       screens: {
         'xxl': '1800px', // สร้างคีย์ xxl สำหรับ 1800px
+      },
+      spacing: {
+        '100px': '100px',
       },
     },
   },
