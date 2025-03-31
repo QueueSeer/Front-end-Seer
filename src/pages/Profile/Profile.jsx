cc1e9c13c78a06a93bc265019292115191983f20
KerkkaiwanMam <kerkkaiwan.ks@gmail.com>
2025-03-31 08:31:37 +0700

    l

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -6,22 +6,21 @@ import UserProfile from "./UserProfile"; // เรียกใช้ path ที
 
 export default function Profile() {
   return (
-    <div className="relative">
-      {/* Navbar - อยู่หน้าสุด */}
-      <div className="h-[68px]">
-        <div className="fixed top-0 left-0 w-full z-[1000]">
-          <Navbar />
-        </div>
+    <div>
+      {/* Navbar - อยู่หน้าสุดโดยใช้ fixed position */}
+      <div className="h-[68px] fixed top-0 left-0 w-full bg-white shadow-md">
+        <Navbar />
       </div>
 
-      <div className="flex px-12 pt-12 gap-14">
-        {/* Sidebar - อยู่หน้ากว่า UserProfile */}
-        <div className="hidden lg:block w-72 lg:sticky lg:top-[88px] lg:self-start z-[900]">
+      {/* คอนเทนต์หลัก ขยับลงมาเพื่อไม่ให้ทับ Navbar */}
+      <div className="flex px-12 pt-20 gap-14">  
+        {/* Sidebar - ใช้ sticky เพื่อให้อยู่ข้างหน้า */}
+        <div className="hidden lg:block w-72 lg:sticky lg:top-[88px] lg:self-start">
           <Sidebar />
         </div>
 
-        {/* UserProfile - อยู่หลังสุด */}
-        <div className="flex-1 pb-10 relative z-[800]">
+        {/* UserProfile - อยู่หลังสุดโดยลำดับโครงสร้าง */}
+        <div className="flex-1 pb-10">
           <UserProfile />
         </div>
       </div>
