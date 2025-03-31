a11d438afbc34ad2799d3e31983424f12163ef21
KerkkaiwanMam <kerkkaiwan.ks@gmail.com>
2025-03-31 08:49:51 +0700

    .

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -6,21 +6,19 @@ import UserProfile from "./UserProfile"; // เรียกใช้ path ที
 
 export default function Profile() {
   return (
-    <div className="min-h-screen flex flex-col">
-      {/* Navbar - อยู่หน้าสุดโดยใช้ fixed position */}
-      <div className="h-[68px] fixed top-0 left-0 w-full bg-white shadow-md z-50">
-        <Navbar />
+    <div>
+      <div className="h-[68px]">
+        <div className="fixed top-0 left-0 w-full z-[999]">
+          <Navbar />
+        </div>
       </div>
-
-      {/* คอนเทนต์หลัก ขยับลงมาเพื่อไม่ให้ทับ Navbar */}
-      <div className="flex flex-1 px-12 pt-20 gap-14">
-        {/* Sidebar - อยู่ข้างหน้าด้วย order และ sticky */}
-        <div className="hidden lg:block w-72 lg:sticky lg:top-[88px] order-1">
+      <div className="flex px-12 pt-12 gap-14">
+        {/* Sidebar - Sticky */}
+        <div className="hidden lg:block w-72 lg:sticky lg:top-[88px] lg:self-start z-50">
           <Sidebar />
         </div>
-
-        {/* UserProfile - อยู่หลังสุด */}
-        <div className="flex-1 pb-10 order-2">
+        {/* คอลัมน์ที่ 2 ใช้พื้นที่ที่เหลือ */}
+        <div className="flex-1 pb-10">
           <UserProfile />
         </div>
       </div>
