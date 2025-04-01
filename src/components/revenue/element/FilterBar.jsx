cd73b31d15f8daed1195717e25eaa3c0f12e31d9
KerkkaiwanMam <kerkkaiwan.ks@gmail.com>
2025-04-02 06:11:32 +0700

    FilterBar

--- src/components/revenue/element/FilterBar.jsx
+++ src/components/revenue/element/FilterBar.jsx
@@ -4,21 +4,6 @@ import DateFilter from './DateFilter';
 const FilterBar = ({ dateFilter, handleDateFilterChange, uniqueMonths }) => {
   return (
     <div className="flex flex-wrap items-center justify-between mb-6 gap-4">
-      <div className="flex items-center space-x-2">
-        <label htmlFor="rows" className="text-gray-700 dark:text-gray-300">
-          แสดง
-        </label>
-        <select
-          id="rows"
-          name="rows"
-          className="border border-gray-300 dark:border-gray-700 rounded-md p-2 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300"
-        >
-          <option value="5">5 แถว</option>
-          <option value="10">10 แถว</option>
-          <option value="15">15 แถว</option>
-        </select>
-        <span className="text-gray-700 dark:text-gray-300">แถว</span>
-      </div>
 
       <DateFilter
         dateFilter={dateFilter}
